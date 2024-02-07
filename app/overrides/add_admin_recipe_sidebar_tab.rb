63044111188deeba491bcd79f7818eed89f9bf40
umairrazam <omairrr.azam@gmail.com>
2024-02-07 13:15:07 +0500

    fix(defaces): header and admin tab

--- app/overrides/add_admin_recipe_sidebar_tab.rb
+++ app/overrides/add_admin_recipe_sidebar_tab.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
-  virtual_path: 'spree/layouts/admin',
+  virtual_path: 'spree/admin/shared/_main_menu',
   name: 'add_admin_recipe_sidebar_tab',
-  insert_bottom: '#main-sidebar',
+  insert_after: '#sidebarProduct',
   partial: 'spree/admin/shared/recipes_sidebar_menu'
 )
--- app/overrides/add_recipes_link_on_header.rb
+++ app/overrides/add_recipes_link_on_header.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
-  virtual_path: 'spree/shared/nav_bar',
+  virtual_path: 'spree/shared/_nav_bar',
   name: 'add_recipes_link_on_header',
-  insert_after: "[data-hook='inside_nav_bar']",
+  insert_top: "[data-hook='inside_nav_bar']",
   partial: 'spree/shared/recipes_nav_bar'
 )
