3044261f2e7896c7af4d00818ac07ba4f573d1ff
umairrazam <omairrr.azam@gmail.com>
2024-02-19 13:23:43 +0500

    fix QA issues

--- app/helpers/spree/frontend_helper_decorator.rb
+++ app/helpers/spree/frontend_helper_decorator.rb
@@ -1,4 +1,32 @@
 Spree::FrontendHelper.class_eval do
+
+  def taxons_tree(root_taxon, current_taxon, max_level = 3)
+    return '' if max_level < 1
+
+    if max_level == 3 && root_taxon.leaf?
+      root_taxon = root_taxon.parent
+    end
+
+    selected_parent_taxon_name = params["id"]
+    show_klass =  (root_taxon.children.where(hide_from_nav: false).pluck("permalink").include?(selected_parent_taxon_name) && root_taxon.parent.present?) ? 'show' : ''
+    parent_klass = (root_taxon.children.where(hide_from_nav: false).present? && ((root_taxon&.parent&.name == "PRODUCTS") || (root_taxon == current_taxon ))) ? 'sidebar-sub-categories' : "dropdown-menu sub-child-manu-js #{show_klass}"
+    
+    content_tag :ul, class: parent_klass   do
+
+      taxons = root_taxon.children.where(hide_from_nav: false).map do |taxon|
+        # if taxon.products.present?
+          taxon_permalink = taxon.permalink
+          selected_taxon_klass = selected_parent_taxon_name == taxon_permalink ? 'active-tab' : ''
+          content_tag :li do
+            css_class = taxon.children.present?  ? 'dropdown-toggle tab-width border-0 p-0' : 'border-0 p-0'
+            link_to(taxon.name, nested_recipe_taxons_path(taxon.permalink.sub("recipes/", "")), data: { turbolinks: false }, 'aria-label': 'Category '+taxon.name, class: selected_taxon_klass) + (taxon.children.any? ? '<button type="button" class="dropdown-toggle js_drop_down" data-toggle="dropdown">Toggle me</button>'.html_safe : '') + taxons_tree(taxon, current_taxon, max_level - 1)
+          end
+        # end
+      end
+      safe_join(taxons, "\n")
+    end
+  end
+
 	def spree_recipe_breadcrumbs(taxon, _separator = '', product = nil)
     return '' if current_page?('/') || taxon.nil?
 
