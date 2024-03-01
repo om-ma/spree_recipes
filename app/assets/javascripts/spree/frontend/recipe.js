ff285b545a1d23b4c2a7fdba2c9ef520857cdeb5
umairrazam <omairrr.azam@gmail.com>
2024-03-01 15:37:43 +0500

    fix staging issue

--- app/assets/javascripts/spree/frontend/recipe.js
+++ app/assets/javascripts/spree/frontend/recipe.js
@@ -6,5 +6,15 @@ $(document).on('turbolinks:load', function() {
   $('#js-recipe-product-modal').on('click', function() {
     $(".recipe-added-modal").show();
   });
+
+  
 });
 
+
+
+
+$('.select2-selection__choice__remove').on('click', function() {
+    var prevSelect = $(this).closest('td').find('select.product_picker')
+    prevSelect.find('option:selected').removeAttr('selected');
+    prevSelect.find('option:selected').val(null)
+});
